d46b561ea4a6116550f67f91789a3f0a2df1452d
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-24 17:01:03 +0545

    Implement OrderController index method and define Order model relationships

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
 
 class OrderController extends Controller
 {
-    //
+    public function index(){
+        $orders= Order::with('user')->paginate(20);
+        Return response()->json($orders,200);
+    }
 }
