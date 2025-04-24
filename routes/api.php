aaadfdfcbd069321d568ed8b6c79576dd9470831
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-24 14:43:52 +0545

    Add category migration to create category table

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,38 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
 // Protected routes (require JWT token)
-Route::middleware('auth:api')->group(function () {
+// Route::middleware('auth:api')->group(function () {
+//     Route::post('logout', [AuthController::class, 'logout']);
+//     Route::get('me', [AuthController::class, 'me']);
+// });
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::get('me', [AuthController::class, 'me']);
+});
+
+
+// brand crud
+Route::controller(BrandController::class)->group(function () {
+    Route::get('index', 'index');
+    Route::get('show/{id}', 'show');
+    Route::post('show/{id}', 'store');
+    Route::put('update_brand/{id}', 'update_brand');
+    Route::delete('delete_brand/{id}', 'delete_brand');
 });
