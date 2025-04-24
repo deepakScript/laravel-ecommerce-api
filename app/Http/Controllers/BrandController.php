16b8b9a80bdf2636e387766049bd21512d36c2b1
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-24 14:34:14 +0545

    Refactor BrandController methods for improved readability and error handling

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Brand;
+use Exception;
 
 class BrandController extends Controller
 {
@@ -13,12 +14,64 @@ class BrandController extends Controller
         return response()->json($brand, 200);
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $brand = Brand::find($id);
-        if($brand){
+        if ($brand) {
             return response()->json($brand, 200);
-        }else{
+        } else {
             return response()->json("Brand not found");
+        }
+    }
+
+
+    public function store(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'name' => 'required|unique:brand,name,'
+            ]);
+
+            $brand = new Brand();
+            $brand->name = $request->name;
+            $brand->save();
+
+            return response()->json('brand Added', 201);
+        } catch (Exception $e) {
+            return response()->json($e, 500);
+        }
+    }
+
+
+    public function update_brand($id, Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'name' => 'required|unique:brand,name,'
+            ]);
+
+            $brand = Brand::where('id', $id)->update([
+                'name' => $request->name
+            ]);
+            return response()->json('brand updated', 200);
+        } catch (\Exception $e) {
+            return response()->json($e, 500);
+        }
+    }
+
+
+    public function delete_brand($id){
+        try {
+            $brand=Brand::where('id',$id);
+            if($brand){
+                $brand->delete();
+                return response()->json('Brand Deleted');
+            }
+            else{
+                return response()->json('Brand not found');
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
         }
     }
 }
